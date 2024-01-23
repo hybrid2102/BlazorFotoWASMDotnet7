9982e87d2c6d9957de8b4e39ce6c61f60d5e8622
Federico Ceteroni <f.ceteroni.ext@teamsystem.com>
2024-01-23 14:31:37 +0100

    efef

--- Shared/Models/FotoForm.cs
+++ Shared/Models/FotoForm.cs
@@ -7,12 +7,5 @@ namespace BlazorFotoWASMDotnet7.Shared.Models
     {
         public Foto? Foto { get; set; }
         public IFormFile? Image {  get; set; }
-
-        public FotoForm() { }  
-        public FotoForm(Foto? foto, IFormFile? image)
-        {
-            Foto = foto;
-            Image = image;
-        }
     }
 }
